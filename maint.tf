31044b48aff0b9281d4176995863ed67940f7989
mlevos <mathias.levostre@gmail.com>
2023-07-20 16:40:40 +0200

    error list subnet

--- maint.tf
+++ maint.tf
@@ -28,8 +28,8 @@ module "eks_cluster" {
   cluster_name    = "devops-toolchain-cluster"
   cluster_version = "1.27"
 
-  vpc_id     = module.vpc.default_vpc_id
-  subnet_ids = module.vpc.module.vpc
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access = true
 
