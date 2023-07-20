69febb146066bdbf1b81db2a5ee1817d42bb531b
mlevos <mathias.levostre@gmail.com>
2023-07-20 16:42:52 +0200

    remove depricated value

--- maint.tf
+++ maint.tf
@@ -33,18 +33,6 @@ module "eks_cluster" {
 
   cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
-
   eks_managed_node_groups = {
     default = {
       min_size     = 1
