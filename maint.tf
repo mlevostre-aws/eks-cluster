5f4f92e644dda2a54224f7511cd7e65e76f443ff
mlevos <mathias.levostre@gmail.com>
2023-07-22 19:19:11 +0200

    fix(): error typo subnet b for the second zone

--- maint.tf
+++ maint.tf
@@ -8,7 +8,7 @@ module "vpc" {
   name = "devops-toolchain-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-3a", "eu-west-3a"]
+  azs             = ["eu-west-3a", "eu-west-3b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
