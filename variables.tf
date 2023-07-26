5ab3394f27c6821535bf05ba85b0026fc61b88c9
Mathias LEVOSTRE <mathias.levostre@gmail.com>
2023-07-26 23:56:37 +0200

    chore(): add devops fargate and remove unused variable (#10)

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "aws_account_id" {
-  description = "AWS account id"
-  type        = string
-  sensitive   = true
-}
 variable "eks_user" {
   description = "which user can connect to eks cluster"
   type        = string
