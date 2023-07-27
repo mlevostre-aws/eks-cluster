d979e378242bbc72b23d30ab9cb96b8332f08b8c
mlevos <mathias.levostre@gmail.com>
2023-07-28 01:32:08 +0200

    fix(): change name ingress-nginx

--- helm.tf
+++ helm.tf
@@ -1,5 +1,7 @@
 resource "helm_release" "nginx_ingress" {
   name       = "ingress-controller"
   repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  depends_on = [ module.eks_cluster ]
 }
