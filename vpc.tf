7c19f6046831d1372d6907ce73ba5ec1ccd5ccec
Mathias LEVOSTRE <mathias.levostre@gmail.com>
2023-07-28 01:01:13 +0200

    fix(): fix region name in vpc (#12)

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "devops-toolchain-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["${data.aws_region.current}a", "${data.aws_region.current}b"]
+  azs             = ["${data.aws_region.current.name}a", "${data.aws_region.current.name}b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
