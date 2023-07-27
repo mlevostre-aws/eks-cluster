090097588726017783b807698252256343141c1d
Mathias LEVOSTRE <mathias.levostre@gmail.com>
2023-07-28 00:18:55 +0200

    chore(): renaming files (#11)

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "devops-toolchain-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${data.aws_region.current}a", "${data.aws_region.current}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
