273b4799b267d2810e6c120abddc3d30eb9e34e9
mlevos <mathias.levostre@gmail.com>
2023-08-11 23:07:25 +0200

    chore(): remove assume role serviceaccount

--- eks.tf
+++ eks.tf
@@ -52,19 +52,3 @@ module "eks_cluster" {
   }
 }
 
-
-module "iam_eks_role" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name = "s3-service-account"
-
-  role_policy_arns = {
-    policy = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  }
-
-  oidc_providers = {
-    s3_spinnaker = {
-      provider_arn               = module.eks_cluster.oidc_provider_arn
-      namespace_service_accounts = ["${kubernetes_namespace.spinnaker.metadata.0.name}:${kubernetes_service_account.spinnaker_service_account.metadata.0.name}"]
-    }
-  }
-}
