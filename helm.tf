db601afae897d9b641a23fd1de7adfcaa2238ce9
mlevos <mathias.levostre@gmail.com>
2023-08-12 20:34:41 +0200

    fix(): add install CRD

--- helm.tf
+++ helm.tf
@@ -2,6 +2,7 @@ resource "helm_release" "nginx_ingress" {
   name       = "ingress-controller"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
+  version    = local.ingress_controler_version
   values = [
     "${file("resources/ingress-values.yaml")}"
   ]
@@ -12,5 +13,10 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
+  version    = local.cert_manager_version
   depends_on = [ module.eks_cluster ]
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
 }
---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  cert_manager_version = "v1.12.3"
+  ingress_controler_version = "1.8.1"
+}
