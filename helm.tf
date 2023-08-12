3c7e6814b16efc106b4a22138aa49863504937ad
mlevos <mathias.levostre@gmail.com>
2023-08-12 20:48:30 +0200

    fix(): add a 'v' to ingress-controller version

--- helm.tf
+++ helm.tf
@@ -2,7 +2,6 @@ resource "helm_release" "nginx_ingress" {
   name       = "ingress-controller"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  version    = local.ingress_controler_version
   values = [
     "${file("resources/ingress-values.yaml")}"
   ]
@@ -13,7 +12,7 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
   chart      = "cert-manager"
-  version    = local.cert_manager_version
+  version    = "v1.12.3"
   depends_on = [ module.eks_cluster ]
   set {
     name  = "installCRDs"
