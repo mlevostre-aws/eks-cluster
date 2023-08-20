61ead4b66a985719d4dc5ed463207e765afed934
Mathias LEVOSTRE <mathias.levostre@gmail.com>
2023-08-21 00:12:22 +0200

    fix(): dependence github namespace helm (#36)

--- utils.tf
+++
@@ -1,9 +0,0 @@
-resource "time_sleep" "wait_helm_runner" {
-/*
-I add this sleep because the runner pods take time to destroy
-I don't find a good way to wait the runner pod destroy 
-that's why i do that
-*/
-  depends_on = [ helm_release.gihtub_action ]
-  destroy_duration = "2m"
-}
