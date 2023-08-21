3f80bf0898ef87bca23dbe91cf414f8309ec3a0b
Mathias LEVOSTRE <mathias.levostre@gmail.com>
2023-08-21 22:46:57 +0200

    feat(): add github action base installation (#41)

--- data.tf
+++ data.tf
@@ -1,2 +1,9 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+data "aws_secretsmanager_secret" "secret_github_app_private_key" {
+  name = var.secret_github_app_private_key
+}
+
+data "aws_secretsmanager_secret_version" "secret_github_app_private_key" {
+  secret_id = data.aws_secretsmanager_secret.secret_github_app_private_key.id
+}
--- kubernetes.tf
+++ kubernetes.tf
@@ -12,6 +12,13 @@ resource "kubernetes_namespace" "application" {
   depends_on = [module.eks_cluster]
 }
 
+resource "kubernetes_namespace" "github" {
+  metadata {
+    name = var.github_namespace_name
+  }
+  depends_on = [module.eks_cluster, module.vpc ]
+}
+
 resource "kubernetes_service_account" "spinnaker_service_account" {
   metadata {
     namespace = kubernetes_namespace.spinnaker.metadata.0.name
