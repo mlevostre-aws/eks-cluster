3ce734be4efc9050de06b3ab7dd089b4663254b7
mlevos <mathias.levostre@gmail.com>
2023-08-21 22:02:25 +0200

    chore(): add variable cluster_name

--- eks.tf
+++ eks.tf
@@ -2,7 +2,7 @@ module "eks_cluster" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = "devops-toolchain-cluster"
+  cluster_name    = var.cluster_name
   cluster_version = "1.27"
 
   vpc_id     = module.vpc.vpc_id
