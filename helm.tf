4f8fd8ee7355a78cb41b17aa8e89e8d98f419ccc
Mathias LEVOSTRE <mathias.levostre@gmail.com>
2023-08-21 21:55:45 +0200

    fix(): change depencies sort (#39)

--- helm.tf
+++ helm.tf
@@ -2,9 +2,6 @@ resource "helm_release" "nginx_ingress" {
   name       = "ingress-controller"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  values = [
-    "${file("resources/ingress-values.yaml")}"
-  ]
   depends_on = [module.eks_cluster]
 }
 
@@ -17,33 +14,5 @@ resource "helm_release" "cert_manager" {
   set {
     name  = "installCRDs"
     value = "true"
-  }
-}
-
-resource "helm_release" "gihtub_action" {
-  // See documentation https://github.com/actions/actions-runner-controller/blob/master/charts/actions-runner-controller/README.md
-  name       = "actions-runner-controller"
-  repository = "https://actions-runner-controller.github.io/actions-runner-controller"
-  chart      = "actions-runner-controller"
-  namespace  = kubernetes_namespace.github.metadata.0.name
-  depends_on = [module.eks_cluster, helm_release.cert_manager]
-  set {
-    name  = "authSecret.create"
-    value = "true"
-  }
-
-  set {
-    name  = "authSecret.github_app_id"
-    value = var.github_app_id
-  }
-
-  set {
-    name  = "authSecret.github_app_installation_id"
-    value = var.github_app_installation_id
-  }
-
-  set {
-    name  = "authSecret.github_app_private_key"
-    value = data.aws_secretsmanager_secret_version.secret_github_app_private_key.secret_string
   }
 }
