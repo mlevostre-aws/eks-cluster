3343ac34cc4fd3d9c0890ed99269c0bb9716fe2c
mlevos <mathias.levostre@gmail.com>
2025-09-16 19:27:21 +0200

    Remove information out of eks definition

--- route53.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_record" "domain" {
-  zone_id = data.aws_route53_zone.domain.zone_id
-  name    = "*.${data.aws_route53_zone.domain.name}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [
-    data.kubernetes_service.ingress_controller_svc.status.0.load_balancer.0.ingress.0.hostname
-  ]
-}
