81d3a15e12531e99fbb619c1cdf40ed30bd5fc64
mlevos <mathias.levostre@gmail.com>
2025-09-16 19:35:40 +0200

    chore: remove info to keep only info for eks

--- data.tf
+++ data.tf
@@ -1,14 +1,5 @@
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
-data "kubernetes_service" "ingress_controller_svc" {
-  metadata {
-    name = local.ingress_controller_svc_name
-  }
-  depends_on = [helm_release.nginx_ingress]
-}
-data "aws_route53_zone" "domain" {
-  name = "${var.domain}."
-}
 
 data "aws_iam_role" "ec2_instance_role" {
   name = var.ec2_instance_role_name
--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "cluster_name" {
   type        = string
 }
 
-variable "domain" {
-  description = "DNS domain"
-  type        = string
-}
-
 variable "ec2_instance_role_name" {
   description = "The assume role instance of the agent"
   type = string
